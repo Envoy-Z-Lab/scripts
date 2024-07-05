2ae32b66685858cd9fe9a7bab35f07db63e23481
Ꮛnѵoყ-Z <152428796+Envoy-Z-Lab@users.noreply.github.com>
2024-07-05 15:03:45 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,48 +1,23 @@
-#!/usr/bin/env bash
-#
-# Script to upload files to GitHub
-#
+#!/bin/bash
 
-# Prompt the user to input the path to the ROM ZIP file
-read -p "Please enter the path to the ROM ZIP: " ROM_ZIP_PATH
+# Define the new copyright text
+NEW_COPYRIGHT="#\n# Copyright (C) 2024 The LineageOS Project\n#\n# SPDX-License-Identifier: Apache-2.0\n#"
 
-# Prompt the user to input the release/tag name
-read -p "Please enter the release/tag name: " TAG
-
-# Prompt the user to input the device name
-read -p "Please enter the device name: " DEVICE
-
-# Define the paths to the files that need to be uploaded
-FILES=(
-    "out/target/product/$DEVICE/boot.img"
-    "out/target/product/$DEVICE/dtbo.img"
-    "out/target/product/$DEVICE/super_empty.img"
-    "out/target/product/$DEVICE/vbmeta.img"
-    "$ROM_ZIP_PATH"
-)
-
-# GitHub repository information
-GH="https://github.com/Envoy-Z-Lab/Releases"
-
-# Function to upload a file to GitHub
-upload_file() {
-    local FP=$1
-    echo -e "Uploading $FP to GitHub..."
+# Function to replace the copyright block in a file
+replace_copyright_in_file() {
+    local file_path="$1"
     
-    # Check if release exists
-    if ! gh release view $TAG --repo $GH > /dev/null 2>&1; then
-        gh release create $TAG --generate-notes --repo $GH
-    fi
-    
-    # Upload the file
-    gh release upload --clobber $TAG $FP --repo $GH
+    # Use sed to replace the copyright block
+    sed -i '/\/\/ Copyright (C) .* The LineageOS Project/,/limitations under the License\./d' "$file_path"
+    sed -i "1i $NEW_COPYRIGHT" "$file_path"
+    echo "Updated copyright in: $file_path"
 }
 
-# Iterate over each file in the FILES array and upload it
-for file in "${FILES[@]}"; do
-    if [ -f "$file" ]; then
-        upload_file "$file"
-    else
-        echo "File $file does not exist, skipping..."
-    fi
-done
+# Export the function to use it with find
+export -f replace_copyright_in_file
+export NEW_COPYRIGHT
+
+# Find all files and apply the replacement function
+find . -type f -exec bash -c 'replace_copyright_in_file "$0"' {} \;
+
+echo "Copyright update completed."
