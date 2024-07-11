e750044689eb4bb70755ccd7a693e3e5e4f3742c
Ꮛnѵoყ-Z <152428796+Envoy-Z-Lab@users.noreply.github.com>
2024-07-11 16:13:33 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -17,8 +17,7 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
 
-# Define the path to the helper script
-HELPER="${HOME}/envoy/derp/tools/extract-utils/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -31,13 +30,7 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 # Warning headers and guards
 write_headers
 
-# Use proprietary-files.txt from sdm660-common
-COMMON_DIR="${MY_DIR}/device/xiaomi/sdm660-common"
-if [ ! -f "${COMMON_DIR}/proprietary-files.txt" ]; then
-    echo "Unable to find proprietary-files.txt at ${COMMON_DIR}/proprietary-files.txt"
-    exit 1
-fi
-write_makefiles "${COMMON_DIR}/proprietary-files.txt" true
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
 # Finish
 write_footers
