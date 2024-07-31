e072387d2439d42b065b71a5efcb76290756ceb8
Ꮛnѵoყ-Z <152428796+Envoy-Z-Lab@users.noreply.github.com>
2024-07-31 22:14:37 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,27 +1,27 @@
-#!/bin/bash
-#
-# Copyright (C) 2017-2019 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+#!/bin/sh
 
-set -e
+OLD_NAME="vhmit"
+OLD_EMAIL="viktor.9630@protonmail.com"
+NEW_NAME="vhmit"
+NEW_EMAIL="viktor.9630@protonmail.com"
 
-# Required!
-export DEVICE=lavender
-export DEVICE_COMMON=sdm660-common
-export VENDOR=xiaomi
+git filter-repo --force --commit-callback '
+import re
 
-export DEVICE_BRINGUP_YEAR=2019
+old_email = b"'"$OLD_EMAIL"'"
+new_email = b"'"$NEW_EMAIL"'"
+old_name = b"'"$OLD_NAME"'"
+new_name = b"'"$NEW_NAME"'"
 
-"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
+if commit.author_email == old_email:
+    commit.author_name = new_name
+    commit.author_email = new_email
+
+if commit.committer_email == old_email:
+    commit.committer_name = new_name
+    commit.committer_email = new_email
+
+# Update co-author entries in commit message
+co_author_pattern = re.compile(rb"^Co-authored-by: .* <" + old_email + rb">", re.MULTILINE)
+commit.message = co_author_pattern.sub(b"Co-authored-by: " + new_name + b" <" + new_email + b">", commit.message)
+' --refs HEAD~135..HEAD
